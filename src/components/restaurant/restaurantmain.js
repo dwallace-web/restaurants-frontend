fd0b3bc70b3a63aa8eca381ecff9cf8a14098be3
David Wallace <dwallacemarketing@gmail.com>
2020-12-19 23:40:47 -0500

    build simple class components

---
+++ src/components/restaurant/restaurantmain.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class restaurantmain extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default restaurantmain
