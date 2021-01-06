7691de78b50dd176eae7578a8f75196551bbb0ad
David Wallace <dwallacemarketing@gmail.com>
2021-01-05 20:18:51 -0500

    comment

--- src/components/restaurant/restaurantmain.js
+++ src/components/restaurant/restaurantmain.js
@@ -4,10 +4,12 @@ export class restaurantmain extends Component {
   render() {
     return (
       <div>
-        <h1>Restaurant Main </h1>
+        <h1>Restaurant Main on master</h1>
       </div>
     );
   }
 }
+
+//comment
 
 export default restaurantmain;
